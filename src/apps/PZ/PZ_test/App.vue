3e2d37469807ed65f6372fcc33efe97988abf437
Porter Zach <porterdzach@gmail.com>
2022-11-21 17:50:26 -0500

    Citation list

--- src/apps/PZ/PZ_test/App.vue
+++ src/apps/PZ/PZ_test/App.vue
@@ -1,18 +1,26 @@
 <template>
-  <div> 
-  <div id="room" class="darkwall">
-  <div class="spacer">
-
-<Title msg="Features in Hubs" />
-<br>
-	<div class="squareoff"> The figure below indicates how to mute your microphone, take photos, share your screen, create media objects, and so on: </div> 
-      <br><br>
-     <div xr-layer>
-       <img  src="../../../assets/images/Onboarding/hubs-user-interface2.jpg" height="212">
-     </div>
+  <div id="room" class="lightwall">
+  <div class="squareoff">
+  <Title msg="Genres of AR and VR: Citations" />
+  <br>[1] <a href="https://www.half-life.com/en/alyx/">Half Life: Alyx</a>, Valve, 2020
+  <br>[2] <a href="https://www.youtube.com/watch?v=R04u9E5INTI">"Pokémon GO - The World of Pokémon GO has Expanded!"</a>, Pokémon GO, 2017
+  <br>[3] <a href="https://www.thewalkingdeadourworld.com/">The Walking Dead: Our World</a>, Next Games, 2013
+  <br>[4] <a href="https://www.youtube.com/watch?v=vL39Sg2AqWg">"Release Trailer | Beat Saber"</a>, Beat Saber Official, 2018
+  <br>[5] <a href="https://www.youtube.com/watch?v=_rYfkDmSRKs">"Minecraft is Now Available on Gear VR!"</a>, Meta Quest, 2016
+  <br>[6] <a href="https://www.steamvr.com/en/">SteamVR</a>, Valve, 2022
+  <br>[7] <a href="https://www.youtube.com/watch?v=ZjTX06DnSEI">"Virtual Reality Art Brought by Christian Lemmerz"</a>, CoBo, 2017
+  <br>[8] <a href="https://www.youtube.com/watch?v=0okmtplObEg">"Klimt's Magic Garden - 360°"</a>, MAK - Museum für angewandte Kunst, Wien, 2018
+  <br>[9] <a href="https://www.youtube.com/watch?v=mk1_0_Tk7C4">"VRLAB @ CPH:DOX"</a>, Makropol Studio, 2016
+  <br>[10] <a href="https://www.youtube.com/watch?v=oJMET0G3ybg">"ACUTE ART | Daniel Birnbaum"</a>, ideaXme, 2022
+  <br>[11] <a href="https://youtu.be/FYP95WJmW3o">"Climbing 1 World Trade Center: Man on Spire | 360 VR Video | The New York Times"</a>, The New York Times, 2016
+  <br>[12] <a href="https://www.youtube.com/watch?v=Ue9P2EbJhCs">"Rec Room - Open Beta Launch Trailer | PS VR"</a>, Playstation, 2017
+  <br>[13] <a href="https://blog.google/products/maps/new-sense-direction-live-view/">"A new sense of direction with Live View"</a>, Mirko Ranieri, 2020
+  <br>[14] <a href="https://www.youtube.com/watch?v=ue2m75G7Ci4">"Advance surgical training with virtual reality | Justin Barad | TEDxBerkeley"</a>, TEDx Talks, 2018
+  <br>[15] <a href="https://www.ikea.com/au/en/customer-service/mobile-apps/say-hej-to-ikea-place-pub1f8af050">"Say hej to IKEA Place"</a>, IKEA, 2022
+  <br>[16] <a href="https://www.youtube.com/watch?v=dijRq7GCRC4">"HOW TO use the 'Omaha Beach' sitsim AR app!"</a>, Gunnar Liestøl, 2018
+  <br>[17] <a href="https://www.youtube.com/watch?v=sShZ0CqLHCk">"Mat Collishaw describes 'Thresholds', his latest VR art work"</a>, TrustNewArt, 2017
+    </div> 
   </div>
-  </div>
-  </div> 
 </template>
 
 <script setup>
@@ -23,6 +31,20 @@ import "../../../assets/room.css"
 </script>
 
 <style scoped>
-
+/* .grad1 {
+  background-image: linear-gradient(to bottom right, #DDD, #999);
+}
+.plaquebold {
+  color: black;
+  text-align: left;
+  font-weight: bold;
+  font-size: 24pt;
+}
+.plaquesub {
+  color: black;
+  text-align: left;
+  font-style: italic;
+  font-size: 16pt;
+  line-height: 0.4;
+} */
 </style>
-
